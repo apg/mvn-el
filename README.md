d95ee05b701386a3b81886de29734e4df983e66c
Andrew Gwozdziewycz <apg@knewton.com>
2013-10-15 14:13:31 -0400

    * initial commit

---
+++ README.md
@@ -0,0 +1,50 @@
+# mvn-el: [Emacs][0] things for [mvn][1]
+
+By Andrew Gwozdziewycz, licensed under the [GNU GPLv3][2]
+
+This is a few helpers for using compilation mode in Emacs with maven.
+
+### Customizations
+
+If you're maven is in a nonstandard location, `(setq mvn-command "/path/to/ant -emacs")`
+
+If you're build doesn't use "pom.xml", you'll need to modify `mvn-command` to end with with `-f` or `--file` and also do `(setq mvn-build-file-name "somethingelse.xml")` so that the automated project root discovery works correctly.
+
+### Installation
+
+Put mvn.el in your `load-path` and add `(require 'mvn)` to your .emacs
+
+### Usage
+
+The basic operation is to invoke `M-x mvn`, which will ask you for a goal.
+
+`M-x mvn-last` will re-issue the last command
+
+`M-x mvn-compile` will run the standard `mvn compile`
+
+`M-x mvn-clean` will run the standard `mvn clean`
+
+`M-x mvn-test` will run the standard `mvn test`
+
+`mvn` can be called non-interactively too, in which case it's called as such: `(mvn "sometask")`. This means that you can can define your own functions like `mvn-compile` for your projects:
+
+    (defun mvn-compile-full ()
+        (interactive)
+        (mvn "dependency:sources"))
+        
+`M-x mvn-kill-cache` kills the internal cache used to speed up the auto-completion of ant tasks in the mini-buffer.
+        
+### Future
+
+In the future, I'd really like to build a more general mode for working with Java code--something like [malabar-mode][3], but without a lot of the semantic stuff it does. Minimal requirements:
+
+    1. It needs to support navigation really well (jtags does a decent to good job here)
+    2. It needs to be able to find code in source jars (if available)
+    3. It needs to have a built in way to launch a REPL (clojure, jython, sisc) for experimental programming purposes
+    4. Run test at point/file
+    5. List possible maven goals
+
+[0]: http://gnu.org/software/emacs
+[1]: http://maven.apache.org
+[2]: http://www.gnu.org/licenses/gpl.html
+[3]: https://github.com/espenhw/malabar-mode
---
+++ mvn.el
@@ -0,0 +1,96 @@
+;;; maven.el --- helpers for compiling with maven
+
+;; Copyright (C) 2013 Andrew Gwozdziewycz <git@apgwoz.com>
+
+;; Version: 0.1
+;; Keywords: compilation, maven, java
+
+;; This file is NOT part of GNU Emacs
+
+;; This is free software; you can redistribute it and/or modify it under
+;; the terms of the GNU General Public License as published by the Free
+;; Software Foundation; either version 3, or (at your option) any later
+;; version.
+
+;; This is distributed in the hope that it will be useful,
+;; but WITHOUT ANY WARRANTY; without even the implied warranty of
+;; MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+;; GNU General Public License for more details.
+
+;; You should have received a copy of the GNU General Public License
+;; along with GNU Emacs; see the file COPYING.  If not, write to the
+;; Free Software Foundation, Inc., 59 Temple Place - Suite 330,
+;; Boston, MA 02111-1307, USA.
+
+(defvar mvn-last-task "compile")
+(defvar mvn-build-file-name "pom.xml")
+(defvar mvn-command "mvn")
+
+(defvar mvn-tasks-default '("compile" "test" "clean"))
+
+(defun mvn-find-tasks (directory)
+  (let ((output (shell-command-to-string (concat *mvn-tasks-command* " "
+                                                 directory "/"
+                                                 mvn-build-file-name))))
+    (message output)
+    (if (> (length output) 0)
+        (mapcar '(lambda (x) (replace-regexp-in-string ".*<target.*name=\"\\([^\-][^\"]*\\).*" "\\1" x)) 
+                (split-string output "[\n]"))
+      nil)))
+
+;; should cache tasks from the build file at some point
+(defun mvn-tasks (directory)
+  (let ((tasks (assoc-string directory *mvn-tasks-cache*)))
+    (cdr 
+     (or tasks
+         (progn 
+           (let ((newtasks (or (mvn-find-tasks directory) mvn-tasks-default)))
+             (setq *mvn-tasks-cache*
+                   (cons (cons directory newtasks) *mvn-tasks-cache*))
+             newtasks))))))
+
+(defun mvn-get-task (directory)
+  (let ((task (completing-read-multiple (concat "Goal (default): ") 
+                                        mvn-tasks-default)))
+    (if (> (length task) 0)
+        (mapconcat 'identity task " ")
+      "")))
+
+(defun mvn-find-root (indicator)
+  (let ((cwd default-directory))
+    (locate-dominating-file cwd mvn-build-file-name)))
+
+(defun mvn-kill-cache ()
+  (interactive)
+  (setq *mvn-tasks-cache* '()))
+
+(defun mvn (&optional task args)
+  "Run mvn `task` in project root directory."
+  (interactive)
+  (let ((default-directory (mvn-find-root mvn-build-file-name)))
+    (if default-directory
+        (let ((task (or task (mvn-get-task default-directory))))
+          (setq mvn-last-task task)
+          (compile (concat mvn-command " " task " " args)))
+      (message "Couldn't find a maven project."))))
+
+(defun mvn-last ()
+  "Run the last maven task in project"
+  (interactive)
+  (mvn (or mvn-last-task "")))
+
+(defun mvn-compile ()
+  (interactive)
+  (mvn "compile"))
+
+(defun mvn-clean ()
+  (interactive)
+  (mvn "clean"))
+
+(defun mvn-test (prefix)
+  (interactive "MTest: ")
+  (if prefix
+      (mvn "test" (concat "-Dtest=" prefix))
+    (mvn "test")))
+
+(provide 'mvn)
